fbfe6e63d7f76472f4f0e77a80a8915c9109b425
Carlos1200 <carlos1200herrera@gmail.com>
2022-04-03 14:29:44 -0600

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,23 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-datocms',
+    options: {
+      "apiToken": "b1151251298b70c57dbf4ff85031ee"
+    }
+  }, "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
