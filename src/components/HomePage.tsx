313d192f87db6ce758facf9584e2d63f8959b9dd
Carlos1200 <carlos1200herrera@gmail.com>
2022-04-10 20:07:50 -0600

    Content absolute

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,33 +5,35 @@ import React from "react";
 export const HomePage = () => {
   return (
     <header>
-      <StaticImage
-        src="../images/paralax.jpg"
-        className="parallaxStyles"
-        alt="paralax"
-      />
-      <div className="parallaxContentStyles">
-        <div>
-          <p className="subTitleStyles">Hello, World.</p>
-          <h1>I'm Carlos Herrera</h1>
-          <p className="textCenter">
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString("Frontend Developer")
-                  .pauseFor(2500)
-                  .deleteAll()
-                  .typeString("Backend Developer")
-                  .pauseFor(2500)
-                  .deleteAll()
-                  .start();
-              }}
-              options={{
-                loop: true,
-              }}
-            />
-          </p>
+      <div className="relative">
+        <div className="parallaxContentStyles">
+          <div>
+            <p className="subTitleStyles">Hello, World.</p>
+            <h1>I'm Carlos Herrera</h1>
+            <p className="textCenter">
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("Frontend Developer")
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .typeString("Backend Developer")
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .start();
+                }}
+                options={{
+                  loop: true,
+                }}
+              />
+            </p>
+          </div>
         </div>
+        <StaticImage
+          src="../images/paralax.jpg"
+          className="parallaxStyles"
+          alt="paralax"
+        />
       </div>
     </header>
   );
